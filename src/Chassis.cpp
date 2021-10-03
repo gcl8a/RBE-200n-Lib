9bd1e6bf81cfd740c444746a477895311b786713
Greg <lewin.gc@gmail.com>
2021-10-03 09:34:48 -0400

    comment

--- src/Chassis.cpp
+++ src/Chassis.cpp
@@ -1,5 +1,11 @@
 #include <Chassis.h>
 
+/**
+ * JUST A TEST FILE FOR THE MOMENT.
+ * 
+ * Don't think this is complete.
+ */
+
 Robot::Robot(void) :
     leftMotor(MOTOR_LEFT_PWM, MOTOR_LEFT_DIR, MOTOR_LEFT_ENCA, MOTOR_LEFT_ENCB),
     rightMotor(MOTOR_RIGHT_PWM, MOTOR_RIGHT_DIR, MOTOR_RIGHT_ENCA, MOTOR_RIGHT_ENCB)
@@ -19,6 +25,6 @@ void Robot::loop(void)
     Serial.print('\t');
 
     delay(20);
-    
+
     leftMotor.setTargetDegreesPerSecond(30);
 }
