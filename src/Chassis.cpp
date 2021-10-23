f79ca7c5694b8529aba2e7045623d515579ad406
Greg <lewin.gc@gmail.com>
2021-10-23 07:34:07 -0400

    renamed Chassis

--- src/Chassis.cpp
+++ src/Chassis.cpp
@@ -1,5 +1,4 @@
 #include "Chassis.h"
-#include "ir_codes.h"
 
 /**
  * WORK IN PROGRESS
@@ -16,7 +15,7 @@ void onMotorTimer(void* param)
 	while(true)
 	{
 		vTaskDelayUntil(&xLastWakeTime, xInterval);
-		robot.MotorHandler();
+		chassis.MotorHandler();
 	}
 	ESP_LOGE(TAG, "ERROR Pid thread died!");
 }
@@ -24,7 +23,7 @@ void onMotorTimer(void* param)
  * @param PWMgenerationTimer the timer to be used to generate the 20khx PWM
  * @param controllerTimer a timer for PID, velocity measurment, and trajectory planning
  */
-void Robot::allocateTimer(int PWMgenerationTimer)
+void Chassis::allocateTimer(int PWMgenerationTimer)
 {
 	if (!timerAllocated)
 	{
@@ -35,7 +34,7 @@ void Robot::allocateTimer(int PWMgenerationTimer)
 	timerAllocated = true;
 }
 
-void Robot::MotorHandler(void)
+void Chassis::MotorHandler(void)
 {
     if(!timerAllocated) allocateTimer(0);
 
@@ -44,67 +43,20 @@ void Robot::MotorHandler(void)
 }
 
 
-Robot::Robot(void) :
+Chassis::Chassis(void) :
     leftMotor(MOTOR_LEFT_PWM, MOTOR_LEFT_DIR, MOTOR_LEFT_ENCA, MOTOR_LEFT_ENCB),
     rightMotor(MOTOR_RIGHT_PWM, MOTOR_RIGHT_DIR, MOTOR_RIGHT_ENCA, MOTOR_RIGHT_ENCB)
 {
     
 }
 
-void Robot::init(void)
+void Chassis::init(void)
 {
 	allocateTimer(0); // used by the DC Motors
 	leftMotor.attach();
     rightMotor.attach();
-
-    mb_ez1.Init(USE_ECHO);// | USE_CTRL_PIN);
-    irDecoder.init(IR_PIN);
 }
 
-void Robot::loop(void)
+void Chassis::loop(void)
 {
-    int16_t keyCode = irDecoder.getKeyCode();
-    if(keyCode != -1) HandleIRPress(keyCode);
-
-    //mb_ez1.CheckPingTimer();
-    uint16_t pulseLen = mb_ez1.CheckEcho();
-    if(pulseLen)
-    {
-        Serial.print(millis());
-        Serial.print('\t');
-        Serial.print(pulseLen);
-        Serial.print('\t');
-        Serial.print(pulseLen * 0.017);
-        Serial.print('\t');
-        
-        wallFollower.ProcessDistanceReading(pulseLen * 0.017);
-
-        Serial.print('\n');
-    }
-}
-
-void Robot::HandleIRPress(int16_t key)
-{
-    Serial.println(key);
-    if(key == INFO)
-    {
-        robotState = ROBOT_IDLE;
-    }
-
-    switch(robotState)
-    {
-        case ROBOT_IDLE:
-            if(key == PREV)
-            {
-                robotState = ROBOT_WALL_FOLLOWING;
-            }
-            break;
-        case ROBOT_DEAD_RECKONING:
-            break;
-        case ROBOT_WALL_FOLLOWING:
-            wallFollower.HandleKeyPress(key);
-            break;
-        default:
-            break;
-    }
 }
--- src/Chassis.h
+++ src/Chassis.h
@@ -3,26 +3,14 @@
 
 #include <RBE-200n-Lib.h>
 #include <MotorEncoded.h>
-#include <wall_follower.h>
-
-#include <event_timer.h>
-#include <button.h>
-
-#include <maxbotix.h>
-#include <IRdecoder.h>
-
-#define IR_PIN 15
 
 /**
  * WORK IN PROGRESS.
  */
 
-class Robot
+class Chassis
 {
 protected:
-    enum ROBOT_STATE {ROBOT_IDLE, ROBOT_DEAD_RECKONING, ROBOT_DRIVING, ROBOT_WALL_FOLLOWING};
-    ROBOT_STATE robotState = ROBOT_IDLE;
-
     MotorEncoded leftMotor;
     MotorEncoded rightMotor;
 
@@ -32,17 +20,14 @@ protected:
 
    	friend void onMotorTimer(void* param);
 
-    WallFollower wallFollower;
-
 public:
-    Robot(void);
+    Chassis(void);
 
     void init(void);
     void loop(void);
 
-    void HandleIRPress(int16_t);
 };
 
-extern Robot robot;
+extern Chassis chassis;
 
 #endif
